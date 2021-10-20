71a41b6e4f89813a018c6c18d71df5f9eeadaec2
stefanolami <stefanolami90@gmail.com>
2021-10-20 14:30:54 +0200

    created the search box code

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+let search = document.querySelector("#search");
+let photos = document.querySelectorAll("a");
+let searchValue = search.value.toLowerCase();
+
+
+search.addEventListener("keydown", () => {
+    let searchValue = search.value.toLowerCase();
+    for (let i = 0; i < photos.length; i++) {
+       if (!photos[i].dataset.caption.includes(searchValue)) {
+           photos[i].style.display = "none";
+       }
+    }
+});
+
+search.addEventListener("keydown", () => {
+    let searchValue = search.value.toLowerCase();
+    for (let i = 0; i < photos.length; i++) {
+       if (photos[i].dataset.caption.includes(searchValue)) {
+           photos[i].style.display = "grid";
+       }
+    }
+});
+
